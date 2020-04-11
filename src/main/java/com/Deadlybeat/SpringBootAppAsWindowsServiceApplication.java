a37f6c4c4d71ea12b7debcb300e5ca9abb1d17ca
Deadlybeat <jadhavakki62@gmail.com>
2020-04-11 12:24:06 +0530

    First Commit

---
+++ src/main/java/com/Deadlybeat/SpringBootAppAsWindowsServiceApplication.java
@@ -0,0 +1,32 @@
+package com.Deadlybeat;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableScheduling
+public class SpringBootAppAsWindowsServiceApplication {
+
+	@GetMapping("/status")
+	public String getStatus() {
+		return "Application is running...!!";
+	}
+	
+	//scheduler to check every 5 sec=5000milisec
+	@Scheduled(fixedDelay = 5000)
+	public void print() {
+		System.out.println("Current time:-"+new Date());
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppAsWindowsServiceApplication.class, args);
+	}
+
+}
